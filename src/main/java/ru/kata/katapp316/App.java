c408c9c6798917c87054e3eefd5c6a7f560108a0
helg893 <helg893@gmail.com>
2024-02-29 00:14:14 +0300

    task complete

--- src/main/java/ru/kata/katapp316/App.java
+++ src/main/java/ru/kata/katapp316/App.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import ru.kata.katapp316.configuration.MyConfig;
 import ru.kata.katapp316.model.User;
 
-import java.util.List;
-
 /**
  * Hello world!
  *
@@ -18,11 +16,24 @@ public class App
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
         Communication communication = context.getBean(Communication.class);
-        List<User> allUsers = communication.getAllUsers();
-        System.out.println(allUsers);
 
-        System.out.println(communication.getCookieSessionId());
+        User user = new User();
+        user.setId(3L);
+        user.setName("James");
+        user.setLastName("Brown");
+        user.setAge((byte)42);
 
+        communication.getAllUsers().forEach(System.out::println);
+        String partOne = communication.createUser(user);
+        user.setName("Thomas");
+        user.setLastName("Shelby");
+        String partTwo = communication.updateUser(user);
+        String partThree = communication.deleteUser(3L);
+
+        System.out.printf("partOne='%s'\n", partOne);
+        System.out.printf("partTwo='%s'\n", partTwo);
+        System.out.printf("partThree='%s'\n", partThree);
+        System.out.printf("in full='%s'\n", partOne + partTwo + partThree);
 
     }
 }
